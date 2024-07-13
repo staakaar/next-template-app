aaaf891488e6577e5fd414444673f3e08707f451
staakaar <vancyroletaka@gmail.com>
2024-07-14 01:27:12 +0900

    fix: tailwindcss load error

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,5 @@
+/** @type {import('tailwindcss').Config} */
+import path from "path";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -5,6 +7,7 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
@@ -14,7 +17,24 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
   },
   plugins: [],
+  important: true,
 };
 export default config;
