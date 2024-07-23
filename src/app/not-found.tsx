ead2123d5f4e4a96b7747117bdb74d3a5306cef1
staakaar <vancyroletaka@gmail.com>
2024-07-24 00:33:44 +0900

    add: boiler template

---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+'use client'
+// NOT FOUNDスロー時の画面
+const NotFound = () => {
+    return <div>Not Found</div>
+}
+
+export default NotFound;
---
+++ src/app/template.tsx
@@ -0,0 +1,11 @@
+'use client'
+import Header from "@/components/organisms/Header";
+
+/** トップページ */
+const TopPage = () => {
+    return (
+        <Header />
+    );
+}
+
+export default TopPage;
