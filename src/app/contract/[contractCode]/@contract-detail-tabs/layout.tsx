865526092e488f797b121431847f06c6276728d1
staakaar <vancyroletaka@gmail.com>
2024-10-12 01:02:49 +0900

    refactor: parallel routes contract detail tab groups

---
+++ src/app/contract/[contractCode]/@contract-detail-tabs/layout.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ContractDetailTabs = ({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) => {
+    return <>{children}</>;
+};
+export default ContractDetailTabs;
--- src/app/contract/[contractCode]/page.tsx
+++ src/app/contract/[contractCode]/page.tsx
@@ -9,9 +9,9 @@ export type ContractDetailsProps = {
 const ContractDetailsPage = () => {
     return (
         <>
-            <Suspense fallback={<VSpinner size="xl" />}>
-                <ContractDetailContainer />
-            </Suspense>
+            {/* <Suspense fallback={<VSpinner size="xl" />}> */}
+            <ContractDetailContainer />
+            {/* </Suspense> */}
             {/* 詳細タブ表示(各ドメイン) */}
             {/* タブに応じて新規作成ページを切り替える 契約書情報を一番最初に入力する必要あり */}
         </>
